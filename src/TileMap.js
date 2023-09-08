93d6b61b1fef437ada971013498e0b92c0850a5b
Geenons david <geenonsdavid@gmail.com>
2023-09-08 14:23:30 +0200

    new code

---
+++ src/TileMap.js
@@ -0,0 +1,75 @@
+export default class TileMap {
+    constructor(tileSize) {
+        this.tileSize = tileSize;
+
+        this.yellowDot = new Image();
+        this.yellowDot.src = './src/assets/yellowDot.png';
+
+        this.pinkDot = new Image();
+        this.pinkDot.src = './src/assets/pinkDot.png';
+
+        this.wall = new Image();
+        this.wall.src = './src/assets/wall.png';
+
+    }
+
+    map = [
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0 ,1],
+        [1, 0, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 0, 1],
+        [1, 3, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 3, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ,1],
+        [1, 0, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 0, 1],
+        [1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1],
+        [1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1],
+        [0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0],
+        [0, 0, 0, 1, 0, 1, 0, 1, 1, 0, 1, 1, 0, 1, 0, 1, 0, 0, 0],
+        [1, 1, 1, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 1, 1, 1],
+        [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+        [1, 1, 1, 1, 0, 1, 0, 1, 1, 0, 1, 1, 0, 1, 0, 1, 1, 1, 1],
+        [0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0],
+        [0, 0, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0, 0, 0],
+        [1, 1, 1, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 1, 1, 1],
+        [1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 0, 1],
+        [1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1],
+        [1, 1, 3, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1, 3, 1, 1],
+        [1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1],
+        [1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    ];
+    draw(ctx) {
+        const tileSize = this.tileSize;
+        for (let i = 0; i < this.map.length; i++) {
+            const row = this.map[i];
+            for (let j = 0; j < row.length; j++) {
+                const tile = row[j];
+                if (tile === 1) {
+                    let wall = new Image();
+                    wall.src = './src/assets/wall.png';
+                    wall.onload = function () {
+                        ctx.drawImage(wall, j * tileSize, i * tileSize, tileSize, tileSize);
+                    };
+                } else if (tile === 0) {
+                    let yellowDot = new Image();
+                    yellowDot.src = './src/assets/yellowDot.png';
+                    yellowDot.onload = function () {
+                        ctx.drawImage(yellowDot, j * tileSize, i * tileSize, tileSize, tileSize);
+                    };
+                } else if (tile === 3) {
+                    let ground = new Image();
+                    ground.src = './src/assets/pinkDot.png';
+                    ground.onload = function () {
+                        ctx.drawImage(ground, j * tileSize, i * tileSize, tileSize, tileSize);
+                    };
+                }
+            }
+    }
+    }
+
+    setCanvasSize(canvas){
+        canvas.width = this.map[0].length * this.tileSize;
+        canvas.height = this.map.length * this.tileSize;
+    };
+}
